fcdce6aecc76fbbd027ef5ba504833ea1eaa1e3a
이현성 <122369935+hyeonseong2023@users.noreply.github.com>
2023-07-21 18:39:59 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
@@ -1,12 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
         
-        if(0<=num1 && num1<=100){
-            if(0<=num2 && num2<=100){
-                answer = num1/num2;
-            }
-        }
-        return answer;
+        return num1/num2;
+        
     }
 }
