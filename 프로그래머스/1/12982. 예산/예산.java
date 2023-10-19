250c0392bae0fc085fc43b4af2c646b111cf78df
이현성 <122369935+hyeonseong2023@users.noreply.github.com>
2023-10-19 15:57:20 +0900

    [level 1] Title: 예산, Time: 1.45 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,10 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         
-        // 1. 낮은 순으로 정렬
-        // 2. 앞에서부터 하나씩 더해가면서 바퀴마다 answer++
-        // 3. 숫자 넘으면 나가고 반환
-        // ** sort, while문 사용
         int answer = 0;
         int sum = 0;
         
@@ -17,13 +13,9 @@ class Solution {
         while(sum<budget){
             sum += d[answer];
             answer++;
-            System.out.println(sum);
         }
         
-        if(sum==budget){
-            return answer;
-        }
-        
+        if(sum==budget) return answer;
         
         return --answer;
     }
