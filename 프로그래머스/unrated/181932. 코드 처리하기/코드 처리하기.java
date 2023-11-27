8680ce509caa9397a59397818dfc5716e03f2f10
이현성 <122369935+hyeonseong2023@users.noreply.github.com>
2023-11-27 15:52:09 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.18 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        String mode = "0";
+        String[] codeArr = code.split("");
+        
+        for(int i = 0; i < codeArr.length; i++){             
+            if(mode.equals("0")){
+                if(!codeArr[i].equals("1")){
+                    if(i % 2 == 0) answer += codeArr[i];
+                }else{
+                    mode = "1";
+                }
+            }else if(mode.equals("1")){
+                if(!codeArr[i].equals("1")){
+                    if(i % 2 == 1) answer += codeArr[i];
+                }else{
+                    mode = "0";
+                }
+            }
+        }
+        
+        if(answer.length() == 0) answer = "EMPTY";
+        
+        return answer;
+    }
+}
