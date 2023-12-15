be17b6660e7169ad3d0a4ed392f426dc31ad5537
이현성 <122369935+hyeonseong2023@users.noreply.github.com>
2023-12-16 07:43:12 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -5,18 +5,15 @@ class Solution {
         int arr22 = 0;
        
         if(arr1.length == arr2.length){
-        for(int i = 0; i<arr1.length; i++){
-            arr11 += arr1[i];
-            arr22 += arr2[i];
-        }
-        
-        if(arr11 == arr22){
-             return 0;
-        }else{
-            return arr11 > arr22 ? 1 : -1; 
-        }
-        }else{
-            return arr1.length>arr2.length ? 1 : -1;              
+            for(int i = 0; i < arr1.length; i++){
+                arr11 += arr1[i];
+                arr22 += arr2[i];
+            }
+
+            return arr11 == arr22 ? 0 : arr11 > arr22 ? 1 : -1; 
+            
+        } else {
+            return arr1.length > arr2.length ? 1 : -1;              
         }
 
     }
