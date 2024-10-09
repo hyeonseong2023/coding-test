4c4bfa9bfcccd2f692701d391652cf37de16ef89
이현성 <122369935+hyeonseong2023@users.noreply.github.com>
2024-10-09 22:39:09 +0900

    [level 0] Title: 리스트 자르기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,8 +1,7 @@
 function solution(n, slicer, num_list) {
     
-    if(n === 1) {
-        return num_list.slice(0, slicer[1] + 1);
-    } else if(n === 2) {
+    if(n === 1) return num_list.slice(0, slicer[1] + 1);
+    else if(n === 2) {
         return num_list.slice(slicer[0]);
     } else if(n === 3) {
         return num_list.slice(slicer[0], slicer[1] + 1);
